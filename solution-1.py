add98086ddf3374e3fd79b7cd5f011d23cb2a965
Wiktoria Wysopal <G00376472@gmit.ie>
2019-03-19 17:35:43 +0000

    solution to problem one with invalid syntax 'n'

---
+++ solution-1.py
@@ -0,0 +1,6 @@
+n = int(input "EnterPlease enter a positive integer: " 
+# user enter a positive integer
+sum = (n(n * n + 1))/2 
+# formula to calculate sum function
+print (sum) 
+# result of sum function
