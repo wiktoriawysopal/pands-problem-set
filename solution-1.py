dbffae3ef84ebfa43df7eb62744a7f251c377fa5
Wiktoria Wysopal <G00376472@gmit.ie>
2019-03-26 18:57:16 +0000

    Reference added to the first problem set

--- solution-1.py
+++ solution-1.py
@@ -2,3 +2,6 @@ n = int (input ("Please enter a positive integer: ")) #user enter a positive int
 
 sum = (n * (n + 1))/2 #formula for the sum function
 print(sum) #result of sum function
+#I had several difficulties with an invalid syntax, which underlined "n" in the first line.
+
+#Reference: https://pynative.com/python-program-to-calculate-sum-and-average-of-numbers/ 
