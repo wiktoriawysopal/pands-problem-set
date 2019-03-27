1392472b128eb8f490dafd26b1f7d81f207d55e8
Wiktoria Wysopal <00376472@gmit.ie>
2019-03-27 19:04:22 +0000

    Solution to problem 2, without comments

--- solution-2.py
+++ solution-2.py
@@ -1,3 +1,7 @@
-#Solution to problem 2
-day = input("What day of the week is today ? ")
-
+n = input("What days of the week is today ? ")
+if n == "Tuesday":
+    print("Yes, today begins with a T.")
+elif n == "Thursday":
+    print("Yes, today begins with a T.")
+else:
+        print("No, today does not begin with a T.")
