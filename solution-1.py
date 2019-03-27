44e44a59a9d560a59f582fc9782a4e7689a33a7f
Wiktoria Wysopal <00376472@gmit.ie>
2019-03-27 19:21:38 +0000

    Solution to problem 1 with users error.

--- solution-1.py
+++ solution-1.py
@@ -1,4 +1,8 @@
 n = int (input ("Please enter a positive integer: ")) #user enter a positive integer
 
 sum = (n * (n + 1))/2 #formula for the sum function
-print(sum) #result of sum function
+print(sum) #result of the sum function
+if n <= 0:
+    print ("This is a negative integer/float, please enter a positive integer.")
+
+#I had several difficulties with an invalid syntax, which underlined n in the first line.
