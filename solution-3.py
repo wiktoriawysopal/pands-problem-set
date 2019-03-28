0a5c1144997bd67ab700d51b892c3a0bd80283d2
Wiktoria Wysopal <00376472@gmit.ie>
2019-03-28 15:57:11 +0000

    Solution to problem 3 with comments

--- solution-3.py
+++ solution-3.py
@@ -1,4 +1,4 @@
-for i in range(1000,10000):
-    if (i % 6 == 0) and not(i % 12 == 0):
-        print(i)
-    
+for i in range(1000,10000): # i represent range between 1,000 and 10,000
+    if (i % 6 == 0) and not(i % 12 == 0): #if statments, with two conditions one is positive and the other is negative, which means that the number is divisible by first condition but not by the second one. Thats why I included "not" before rounded brackets.
+        print(i) #the if statment returns all the values divisible by 6, but not 12.
+
