bc102f23982413f4e258958240d6cfcdad9ecb90
Wiktoria Wysopal <00376472@gmit.ie>
2019-03-30 18:06:47 +0000

    Final draft of solution 4, with syntax error

--- solution-4.py
+++ solution-4.py
@@ -1,3 +1,9 @@
 #Empty file, collatz conjencture research
 n = int(input("Please enter a positive integer ")) #The oscar ask user to enter a positive integer
 print(n) #The output of the program
+while n != 1: #while loop is used, to iterate until it meets the condition of reaching the value of 1
+
+if(n % 2 == 0): #if the integer is an even number, then divide it by 2
+        print (n/2) #print the output of the division
+else(n*3) + 1: #if the integer is odd number, then multiply by 3 and add 1
+        print (n) #print the output of the multiplication
