0da6e4d186fda40138a4fcc344adcd5b065d8442
Wiktoria Wysopal <00376472@gmit.ie>
2019-03-31 14:48:12 +0100

    New code for solution4

--- solution-4.py
+++ solution-4.py
@@ -3,5 +3,5 @@ print(n) #The output of the program
 while n != 1: #while loop is used, to iterate until it meets the condition of reaching the value of 1    
     if(n % 2 == 0): #if the integer is an even number, then divide it by 2 
         print (n/2) #print the output of the division
-    else(n*3) + 1: #if the integer is odd number, then multiply by 3 and add 1
+else(n*3) + 1: #if the integer is odd number, then multiply by 3 and add 1
         print (n) #print the output of the multiplication
