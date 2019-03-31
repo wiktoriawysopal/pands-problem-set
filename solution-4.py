dc17abe749df5a6d983d5d4028e1a7a47595a16e
Wiktoria Wysopal <00376472@gmit.ie>
2019-03-31 14:58:25 +0100

    Solution to problem 4 with comments

--- solution-4.py
+++ solution-4.py
@@ -1,7 +1,8 @@
 n = int(input("Please enter a positive integer ")) #user enter a positive integer
-print(n) #The output of the program 
-while n != 1: #while loop is used, to iterate until it meets the condition of reaching the value of 1    
-    if(n % 2 == 0): #if the integer is an even number, then divide it by 2 
-        print (n/2) #print the output of the division
-else(n*3) + 1: #if the integer is odd number, then multiply by 3 and add 1
-        print (n) #print the output of the multiplication
+while n != 1: # check for loop to infinity
+    if n % 2 == 0: #if the number is even
+        n = n/2
+        print(n) 
+    else: #if the number is odd
+        n = (n * 3) + 1
+        print(n)
