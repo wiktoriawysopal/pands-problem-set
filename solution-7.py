138f3fe1f3997912e69b14dc5abea3794ed1e006
Wiktoria Wysopal <00376472@gmit.ie>
2019-04-22 12:40:40 +0100

    Solution to problem 7 without comments

--- solution-7.py
+++ solution-7.py
@@ -1,7 +1,6 @@
 #The program calculates the approximation of the square root of a positive floating point number
 #The number to calculate the square root
 n = int(input("Please enter a positive floating point number"))
-
 def newtonSqrt(n):
     estimate = 0.5 * n
     better = 0.5 * (estimate + n/estimate)
